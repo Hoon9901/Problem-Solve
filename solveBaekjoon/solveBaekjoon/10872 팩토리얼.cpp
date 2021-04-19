f8c9ae79741ef8103aa90f3bee20b541bd5b5c1d
Hooooooon <shonn2323@gmail.com>
2021-04-19 18:01:48 +0900

    10872 팩토리얼

---
+++ solveBaekjoon/solveBaekjoon/10872 팩토리얼.cpp
@@ -0,0 +1,44 @@
+﻿#include <bits/stdc++.h>
+/*
+#include <iostream>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <algorithm>
+*/
+using namespace std;
+
+#define ONLINE_JUDGE		// if you use online judge you must write this code
+
+#define hIn(x) cin >> (x)
+#define hCin(x) int (x); cin >> (x)
+#define hOut(x) cout << x << '\n'
+#define hOutBlank(x) cout << x << ' '
+#define hforOut(arr,size) for(int i = 0; i < size; i++) { cout << arr[i] << '\n';}
+#define hforIn(arr, size) for(int i = 0 ; i < size; i++) cin >> arr[i];
+
+#define delarr(x) delete [] x
+#define pairInt pair<int, int>
+#define tlong long long
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+#ifndef  ONLINE_JUDGE	// ONLINE_JUDGE 라는 값이 없으면 endif까지 아래 코드를 사용.
+	freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);	// 출력이 너무 길때 사용
+#endif // ! ONLINE_JUDGE
+	int N; // n <= 45
+	hIn(N);
+	
+	tlong result = 1;
+	for (int i = 1; i <= N; i++)
+	{
+		result *= i;
+	}
+	cout << result;
+	return 0;
+}
