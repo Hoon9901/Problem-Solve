b7e7a1b15f396b2360499dd38896c2218fa79478
Hooooooon <shonn2323@gmail.com>
2021-04-19 17:58:46 +0900

    2748 배열을 이용한 피보나치수 (90개)

---
+++ solveBaekjoon/solveBaekjoon/2748 피보나치수 2.cpp
@@ -0,0 +1,45 @@
+﻿#include <bits/stdc++.h>
+/*
+#include <iostream>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <algorithm>
+*/
+using namespace std;
+
+#define ONLINE_JUDGE		// if you use online judge you must write this code
+
+#define hIn(x) cin >> (x)
+#define hCin(x) int (x); cin >> (x)
+#define hOut(x) cout << x << '\n'
+#define hOutBlank(x) cout << x << ' '
+#define hforOut(arr,size) for(int i = 0; i < size; i++) { cout << arr[i] << '\n';}
+#define hforIn(arr, size) for(int i = 0 ; i < size; i++) cin >> arr[i];
+
+#define delarr(x) delete [] x
+#define pairInt pair<int, int>
+#define tlong long long
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+#ifndef  ONLINE_JUDGE	// ONLINE_JUDGE 라는 값이 없으면 endif까지 아래 코드를 사용.
+	freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);	// 출력이 너무 길때 사용
+#endif // ! ONLINE_JUDGE
+	int n; // n <= 45
+	hIn(n);
+	tlong* fibo;
+
+	fibo = new tlong[n + 1];
+	fibo[0] = 0, fibo[1] = 1;
+	for (int i = 2; i <= n; i++) {
+		fibo[i] = fibo[i - 1] + fibo[i - 2];
+	}
+	cout << fibo[n];
+	delete[] fibo;
+	return 0;
+}
