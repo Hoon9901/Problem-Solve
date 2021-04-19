a183b2f9feb685d93e058f948a5315d93a7bc245
Hooooooon <shonn2323@gmail.com>
2021-04-19 18:07:00 +0900

    2501 약수 구하기

---
+++ solveBaekjoon/solveBaekjoon/2501 약수 구하기.cpp
@@ -0,0 +1,50 @@
+﻿#include <bits/stdc++.h>
+/*
+#include <iostream>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <algorithm>
+*/
+using namespace std;
+
+#define ONLINE_JUDGE		// if you use online judge you must write this code
+
+#define hIn(x) cin >> (x)
+#define hCin(x) int (x); cin >> (x)
+#define hOut(x) cout << x << '\n'
+#define hOutBlank(x) cout << x << ' '
+#define hforOut(arr,size) for(int i = 0; i < size; i++) { cout << arr[i] << '\n';}
+#define hforIn(arr, size) for(int i = 0 ; i < size; i++) cin >> arr[i];
+
+#define delarr(x) delete [] x
+#define pairInt pair<int, int>
+#define tlong long long
+
+int getDivisor(int num, int target) {
+	int count = 0;
+	for (int i = 1; i <= num; i++) {
+		if (num % i == 0) {
+			count++;
+		}
+		if (count == target)
+			return i;
+	}
+	if (count < target)
+		return 0;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+#ifndef  ONLINE_JUDGE	// ONLINE_JUDGE 라는 값이 없으면 endif까지 아래 코드를 사용.
+	freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);	// 출력이 너무 길때 사용
+#endif // ! ONLINE_JUDGE
+	int N, K; // n <= 45
+	hIn(N),	hIn(K);
+	cout << getDivisor(N, K);
+	return 0;
+}
