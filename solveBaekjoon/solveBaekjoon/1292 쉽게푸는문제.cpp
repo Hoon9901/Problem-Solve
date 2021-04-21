ac0f20dcc22141d4bb3c0a0d63c3e3e2e5dcbc0c
Hooooooon <shonn2323@gmail.com>
2021-04-21 21:30:33 +0900

    1292

---
+++ solveBaekjoon/solveBaekjoon/1292 쉽게푸는문제.cpp
@@ -0,0 +1,52 @@
+﻿#include <bits/stdc++.h>
+/*
+#include <iostream>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <algorithm>
+*/
+using namespace std;
+
+#define ONLINE_JUDGE		// if you use online judge you must write this code
+
+#define hIn(x) cin >> (x)
+#define hCin(x) int (x); cin >> (x)
+#define hOut(x) cout << x << '\n'
+#define hOutBlank(x) cout << x << ' '
+#define hforOut(arr,size) for(int i = 0; i < size; i++) { cout << arr[i] << '\n';}
+#define hforIn(arr, size) for(int i = 0 ; i < size; i++) cin >> arr[i];
+
+#define delarr(x) delete [] x
+#define pairInt pair<int, int>
+#define tlong long long
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+#ifndef  ONLINE_JUDGE	// ONLINE_JUDGE 라는 값이 없으면 endif까지 아래 코드를 사용.
+	freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);	// 출력이 너무 길때 사용
+#endif // ! ONLINE_JUDGE
+	int start, end;
+	hIn(start), hIn(end);
+
+	int cnt = 0;
+	int i = 1;
+	int sum = 0;
+	while (cnt != end) {
+		for (int j = 0; j < i; j++) {
+			cnt++;
+			if(cnt >= start)
+				sum += i;
+			if (cnt == end)
+				break;
+		}
+		i++;
+	}
+	cout << sum;
+
+	return 0;
+}
