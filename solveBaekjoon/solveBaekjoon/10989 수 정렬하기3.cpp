c6f8e74be8b2bfadce2472c330d0ca25303e95eb
Hooooooon <shonn2323@gmail.com>
2021-04-21 22:42:04 +0900

    10989 카운팅 정렬

---
+++ solveBaekjoon/solveBaekjoon/10989 수 정렬하기3.cpp
@@ -0,0 +1,49 @@
+﻿#include <bits/stdc++.h>
+/*
+#include <iostream>
+#include <deque>
+#include <vector>
+#include <queue>
+#include <algorithm>
+*/
+using namespace std;
+
+#define ONLINE_JUDGE		// if you use online judge you must write this code
+
+#define hIn(x) cin >> (x)
+#define hCin(x) int (x); cin >> (x)
+#define hOut(x) cout << x << '\n'
+#define hOutBlank(x) cout << x << ' '
+#define hforOut(arr,size) for(int i = 0; i < size; i++) { cout << arr[i] << '\n';}
+#define hforIn(arr, size) for(int i = 0 ; i < size; i++) cin >> arr[i];
+
+#define delarr(x) delete [] x
+#define pairInt pair<int, int>
+#define tlong long long
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+#ifndef  ONLINE_JUDGE	// ONLINE_JUDGE 라는 값이 없으면 endif까지 아래 코드를 사용.
+	freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);	// 출력이 너무 길때 사용
+#endif // ! ONLINE_JUDGE
+	int N;
+	hIn(N);
+	int count[10001] = { 0 };
+
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		count[temp] += 1;
+	}
+	for (int i = 0; i < 10001; i++) {
+		// 숫자의 개수 출력
+		for (int j = 0; j < count[i]; j++) {
+			cout << i << '\n';
+		}
+	}
+	return 0;
+}
