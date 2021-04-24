f3c6d632e2431734c70e06aaa3200aa2bdaa449e
Hooooooon <shonn2323@gmail.com>
2021-04-24 23:55:28 +0900

    갱신

--- solveBaekjoon/solveBaekjoon/solveBaekjoon.cpp
+++ solveBaekjoon/solveBaekjoon/solveBaekjoon.cpp
@@ -21,35 +21,6 @@ using namespace std;
 #define pairInt pair<int, int>
 #define tlong long long
 
-/*
-* 전역변수
-*/
-int N, M;			// N : 카드 개수 M : 목적지
-int cards[101];		// 카드가 쓰여 있는 수의 집합
-int result;
-
-//int userCards[3];
-void findCard() {
-	int range = 3000001;
-	for (int i = 0; i < N - 2; i++) {
-		int sum = cards[i];
-		// 카드 3개를 택한다
-		for (int j = i+1; j < N - 1; j++){
-			for (int k = j + 1; k < N; k++) {
-				sum = cards[i] + cards[j] + cards[k];
-				// M에 가까울때..
-				if (M - sum < range && M - sum >= 0) {
-					range = M - sum;
-					result = sum;
-				}
-			}
-		}
-		if (range == 0)	// 바로 종료
-			break;
-	}
-	hOut(result);
-}
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
@@ -59,13 +30,36 @@ int main()
 	freopen("input.txt", "r", stdin);
 	//freopen("output.txt", "w", stdout);	// 출력이 너무 길때 사용
 #endif // ! ONLINE_JUDGE
-	hIn(N);			// 3 <= N <= 100
-	hIn(M);			// 10 <= M <= 300,000 
-	// N장의 카드에 써져 있는 숫자가 주어졌을 때, M을 넘지 않으면서 
-	// M에 최대한 가까운 카드 3장의 합을 구해 출력하시오.
-	hforIn(cards, N);
+	int N;
+	hIn(N); // 1 <= n <= 100,000S
 
-	findCard();
+	queue <int> seq; // 목표 수열
+
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		seq.push(temp);
+	}
+	// 1부터 N 까지
+	int i = 1;
+	stack<int> m_stack; // 스택
+	queue<int> result;// 결과
+	
+
+	do{
+		m_stack.push(i);
+		if (m_stack.top() == seq.front()) {
+			result.push(i);
+			
+			m_stack.pop();
+			seq.pop();
+		}
+		else {
+			i++;
+		}
+		cout << m_stack.top() << endl;
+		
+	} while (!m_stack.empty());
 
 	return 0;
 }
